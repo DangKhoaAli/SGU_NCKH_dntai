f20ba82ff8b87afe4849b4777709703c4a82e203
Phatle-2005 <thanhphat20502@gmail.com>
2026-04-30 21:08:48 +0700

    Tesst

--- modules/loss.py
+++ modules/loss.py
@@ -8,8 +8,8 @@ class LanguageModelCriterion(nn.Module):
 
     def forward(self, input, target, mask):
         # truncate to the same size
-        target = target[:, :input.size(1)]
-        mask = mask[:, :input.size(1)]
+        target = target[:, :input.size(1)].to(input.device)
+        mask = mask[:, :input.size(1)].to(input.device)
         output = -input.gather(2, target.long().unsqueeze(2)).squeeze(2) * mask
         output = torch.sum(output) / torch.sum(mask)
         return output
@@ -30,8 +30,8 @@ def compute_nll_sum_and_tokens(output, reports_ids, reports_masks):
     target = reports_ids[:, 1:]
     mask = reports_masks[:, 1:]
 
-    target = target[:, :output.size(1)]
-    mask = mask[:, :output.size(1)].float()
+    target = target[:, :output.size(1)].to(output.device)
+    mask = mask[:, :output.size(1)].float().to(output.device)
 
     nll = -output.gather(2, target.long().unsqueeze(2)).squeeze(2)
     nll = nll * mask
