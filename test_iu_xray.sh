4e86bd116fec78935e8121f8e02dc0e85ee0aad9
Khoa <khoanguyen12062005@gmail.com>
2026-05-09 11:53:39 +0700

    .

--- test_iu_xray.sh
+++ test_iu_xray.sh
@@ -1,13 +1,16 @@
+MODEL_NAME="${MODEL_NAME:-}"
+DATASET_PATH="${DATASET_PATH:-data/iu_xray}"
+
 python main_test.py \
---image_dir data/iu_xray/images/ \
---ann_path data/iu_xray/annotation.json \
---dataset_name iu_xray \
---max_seq_length 60 \
---threshold 3 \
---batch_size 16 \
---epochs 100 \
---save_dir results/iu_xray \
---step_size 50 \
---gamma 0.1 \
---seed 9223 \
---load data/model_iu_xray.pth
+    --image_dir "$DATASET_PATH/iu_xray/images/" \
+    --ann_path "$DATASET_PATH/iu_xray/annotation.json" \
+    --dataset_name iu_xray \
+    --max_seq_length 60 \
+    --threshold 3 \
+    --batch_size 16 \
+    --epochs 100 \
+    --save_dir results/iu_xray \
+    --step_size 50 \
+    --gamma 0.1 \
+    --seed 9223 \
+    --load "$MODEL_NAME" \
